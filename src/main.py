644bd83200b93b08797deaa88b67167d3275028f
SHIVASANNIDH <144987564+SHIVASANNIDH@users.noreply.github.com>
2025-09-24 14:29:44 +0530

    main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
+from src.graph import run_travel_planner
 
+if __name__ == "__main__":
+    query = "Plan a 2-day trip to New Delhi starting tomorrow"
+    output = run_travel_planner(query)
+
+    print("\n=== Travel Plan Output ===")
+    print(output)
